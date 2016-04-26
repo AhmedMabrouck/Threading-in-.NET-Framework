8b06a14cb068e5524aedf730c2c10d96e098e311
Ahmed <Ahmed@DESKTOP-123VPQS>
2016-04-26 15:22:26 +0200

    Creating Repository

---
+++ 01.Creating Simple Thread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CreatingSimpleThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(WriteY);  // Create new thread calls WriteY method.
+            thread.Start();                      // Start the thread.
+
+            WriteX();                            // Call WriteX method in the main thread.
+
+            // Conclusion: the CPU switched between threads using its own technique 
+            // to run them simultaneously.
+        }
+
+        static void WriteX()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("X");
+        }
+
+        static void WriteY()
+        {
+            for (int i = 0; i < 1000; i++) Console.Write("Y");
+        }
+    }
+}
---
+++ 02.Separate Memory Stack/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02.Separate_Memory_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(DoWork);   // Create new thread calls DoWork method.
+            thread.Start();                       // Start the thread.
+
+            DoWork();                             // Call DoWork method in the main thread.
+
+            // Conclusion: "Doing Work!" is printed 10 times not 5 which means
+            // that each thread has it own separate memory stack for its locals.
+        }
+
+        static void DoWork()
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine("Doing Work!");
+        }
+    }
+}
